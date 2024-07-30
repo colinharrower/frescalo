71d1b850be2e07f4fae443f3c6cbadcac7430342
Colin Harrower <corr@ceh.ac.uk>
2024-07-30 17:10:25 +0100

    beginning to add help text for fresc_scaling function

--- R/fresc_scaling.R
+++ R/fresc_scaling.R
@@ -1,3 +1,66 @@
+#' Determine Frequency Scaling Factor Based on Local Occupancy
+#'
+#' @param neigh_wts Neighbourhood Weights data
+#' @param spp_pa Presence absence data for each species
+#' @param all_loc Vector containing all unique locations in the occupany data
+#' @param all_spp vector containing all unique taxa or species names in the
+#'   occupancy data
+#' @param Phi The target neighbourhood frequency. The default is `Phi = 0.74`.
+#' @param R_star The benchmarking species rank proportion. The default `R_star =
+#'   0.2703` means that after correction for recording effort the top 27% of
+#'   species are used as benchmarks.
+#' @param missing_data action to take when locations in neighbourhoods are
+#'   missing from the occupancy dataset. The default `missing_data = 2` assumes
+#'   that all species are absent or not recorded from the missing locations,
+#'   which . The alternative `missing_data = 1` will exclude any neighbourhoods
+#'   that contain locations missing from the occupancy data.
+#'
+#' @return A list comprised of two elements. The first element `locs` contains a
+#'   `data.frame` with the location metrics while the the second `freq` contains
+#'   a `data.frame` with the re-scaled species frequencies. These two outputs
+#'   are equivalent to `samples.txt` and `frequencies.out` output files produced
+#'   by the original frescalo fortran program.
+#'
+#'   The data.frame in `locs` has a row for each location in `spp_pa` with the
+#'   following columns:
+#'   \item{location}{the location name(s) or identifier(s)}
+#'   \item{nSpecies}{the total number of species or taxa observed}
+#'   \item{phi_in}{the `phi_i`, i.e. local frequency-weighted mean frequency,
+#'   for the local neighbourhood for the given location}
+#'   \item{alpha}{the
+#'   rescaling factor used to adjust the resepective neighbourhood `phi` so that
+#'   it matches the target `phi` specied in the `Phi` argument to the function}
+#'   \item{phi_out}{the final `phi` after rescaling, which should match the
+#'   value supplied to the input argument `Phi `}
+#'   \item{spnum_in}{}
+#'   \item{iter}{the number of iterations required to determine the scaling
+#'   factor `alpha` required}
+#'
+#'   The data.frame in `freq` has rows for each location taxa combination with
+#'   the following columns:
+#'   \item{location}{the location name(s) or identifier(s)}
+#'   \item{species}{the species/taxa name(s) or identifier(s)}
+#'   \item{pres}{logical determining prescence (`1`) or absence (`0`) of the
+#'   species/taxa at that location}
+#'   \item{freq}{the localisied frequency for that species & neighbourhood before rescaling}
+#'   \item{freq_1}{the local frequency for that species & neighbourhood after rescaling}
+#'   \item{rank}{the original frequency rank}
+#'   \item{rank_1}{the frequency rank after rescaling}
+#'   \item{benchmark}{indicates whether the species was a benchmark
+#'   species for the local neighbourhood (`1` = benchmark
+#'   species/taxa)}
+#'
+#' @export
+#'
+#' @description Determines the frequency scaling factors required in order to
+#'   attempt to correct for variation in recording effort.
+#'
+#'
+#' @examples
+#'
+#'
+#'
+#'
 fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0.2703, missing_data = 2){
   # This function calculates the sampling effort multiplier, alpha, that would equate sampling effort across all regions
   # This is the main method of Frescalo.
@@ -39,9 +102,9 @@ fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0
     # If data missing for any neighbourhood locations for current focal location assign (e.g. nomatch) give it a index 1 greater than length of (spp_pa) so it will have a NULL value in speciesRegional
     neighbourhood = match(focal_d$location2, all_loc, nomatch=length(spp_pa)+1) # empty neighbourhood sites are NULL
     speciesRegional = spp_pa[neighbourhood]
-    speciesRegional <- lapply(speciesRegional, function(x) if(is.null(x)) rep(0, times = length(all_spp)) else x) # Find any locations in the neighbourhood that are not in spp_pa (should be NULL in speciesRegional) and create a spp_pa like vector for them with zeros for every species
+    speciesRegional = lapply(speciesRegional, function(x) if(is.null(x)) rep(0, times = length(all_spp)) else x) # Find any locations in the neighbourhood that are not in spp_pa (should be NULL in speciesRegional) and create a spp_pa like vector for them with zeros for every species
 
-    missingData <- neighbourhood==length(spp_pa)+1 # length(spp_pa) is number of locations; missing data is neighbourhoods with
+    missingData = neighbourhood==length(spp_pa)+1 # length(spp_pa) is number of locations; missing data is neighbourhoods with
     if (any(missingData) & missing_data==1) {
       # Species data missing from a location in the neighbourhood. Ignore this focal location
       warning(paste('Removing location ',focal,'. Missing data for locations in the neighbourhood.', sep=''))
@@ -56,18 +119,18 @@ fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0
       # Create weighted neighbourhood frequencies (checked against Frescalo)
       frequency = Reduce('+',Map('*',as.list(weights), speciesRegional))
       # Added by Oli Pescott, Apr 2024, to avoid zeros and match Hill (2012) outputs
-      frequency <- ifelse(frequency==0, frequency+1.0E-10, frequency)
+      frequency = ifelse(frequency==0, frequency+1.0E-10, frequency)
       phi_in = sum(frequency^2) / sum(frequency)
 
       # Calculate the multiplier (alpha) that equalises recording effort
       alpha_min = 1  # Minimum alpha ( =1 means no correction required)
       alpha_max = 5
-      # Increase alpha_max until min.fun() becomes positive (i.e. ensure there is a zero)
-      while (min.fun(alpha_max, frequency, Phi)<0) { alpha_max = alpha_max + 5}
-      while (min.fun(alpha_min, frequency, Phi)>0) { alpha_min = alpha_min/2}
+      # Increase alpha_max until freq_min_f() becomes positive (i.e. ensure there is a zero)
+      while (freq_min_f(alpha_max, frequency, Phi)<0) { alpha_max = alpha_max + 5}
+      while (freq_min_f(alpha_min, frequency, Phi)>0) { alpha_min = alpha_min/2}
 
       # Find sampling-effort multiplier
-      sol=uniroot(min.fun,interval=c(alpha_min,alpha_max), tol=0.0003, frequency, Phi)
+      sol=uniroot(freq_min_f,interval=c(alpha_min,alpha_max), tol=0.0003, frequency, Phi)
       out_loc$alpha[i_f] = sol$root
       out_loc$phi_in[i_f] = phi_in
       out_loc$iter[i_f] = sol$iter
@@ -103,5 +166,16 @@ fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0
   # Collapse temp_freq to single data.frame
     out_freq = do.call("rbind",temp_freq)
   # Return outputs as list
-  return(list(frescalo=out_loc, freq=out_freq))
+  return(list(locs=out_loc, freq=out_freq))
+}
+
+freq_min_f <- function(alpha,fij,Phi){
+  # The function to minimise to fit Frescalo model to the data
+  # Page 5 column 2 in Hill (2011)
+  fr=1-(1-fij)^alpha
+  #fr[abs(fij-1)<1e-10] = 1 # Yearsley
+  fr[abs(fij-1)<1.0E-10] = 0.99999 # to match Hill (2012), although not needed for Yearlsey calc strictly
+  fr[abs(fij-1)>0.99999] = 1.0E-10
+  out = sum(fr^2)/sum(fr) - Phi
+  return(out)
 }
