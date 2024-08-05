3a712588eaf51f3491e92f50192bdb63e368af70
Colin Harrower <corr@ceh.ac.uk>
2024-08-05 18:43:55 +0100

    some changes to fresc_scaling to try and catch some issues when fitting to sparse unicorns data

--- R/fresc_scaling.R
+++ R/fresc_scaling.R
@@ -1,25 +1,38 @@
 #' Determine Frequency Scaling Factor Based on Local Occupancy
 #'
 #' @param neigh_wts Neighbourhood Weights data
-#' @param spp_pa Presence absence data for each species
-#' @param all_loc Vector containing all unique locations in the occupany data
+#' @param spp_pa list specifying presence or absence of species at each location.
+#'   The list should correspond to `all_loc` where the ith element of `spp_pa` list
+#'   is the presence/absence data for ith location in `all_loc`.
+#'   Each element of `spp_pa` should contain a vector of 0 or 1 values of length =
+#'   `length(all_spp)` where 0 represents absence and 1 presence of that given
+#'   species in the given location. The order of the presence and absence data
+#'   should match the order of species in `all_spp`
+#' @param all_loc Vector containing all unique locations in the occupancy data.
+#'   The order should match the order of locations in `spp_pa`.
 #' @param all_spp vector containing all unique taxa or species names in the
-#'   occupancy data
+#'   occupancy data. The order should match the order of species presence and
+#'   absence data in `spp_pa`.
 #' @param Phi The target neighbourhood frequency. The default is `Phi = 0.74`.
 #' @param R_star The benchmarking species rank proportion. The default `R_star =
 #'   0.2703` means that after correction for recording effort the top 27% of
 #'   species are used as benchmarks.
+#' @param filter_wts Logical value determining whether neigh_wts should be filtered
+#'   to only use neighbourhood info for locations in the occupancy data. The
+#'   default `filter_wts = FALSE` will attempt to determine statistics for every
+#'   focal location in `neigh_wts` while `TRUE` will exclude any focal locations
+#'   in `neigh_wts` that are not appear in the occupancy data.
 #' @param missing_data action to take when locations in neighbourhoods are
 #'   missing from the occupancy dataset. The default `missing_data = 2` assumes
 #'   that all species are absent or not recorded from the missing locations,
 #'   which . The alternative `missing_data = 1` will exclude any neighbourhoods
 #'   that contain locations missing from the occupancy data.
 #'
-#' @return A list comprised of two elements. The first element `locs` contains a
-#'   `data.frame` with the location metrics while the the second `freq` contains
-#'   a `data.frame` with the re-scaled species frequencies. These two outputs
-#'   are equivalent to `samples.txt` and `frequencies.out` output files produced
-#'   by the original frescalo fortran program.
+#' @return A list comprised of two elements each containing a data.frame. The
+#'   first element `locs` contains location metrics while the the second `freq`
+#'   contains the original and re-scaled species frequencies. These two
+#'   outputs are equivalent to `samples.txt` and `frequencies.out` output files
+#'   produced by Mark Hill's original frescalo fortran program.
 #'
 #'   The data.frame in `locs` has a row for each location in `spp_pa` with the
 #'   following columns:
@@ -42,18 +55,24 @@
 #'   \item{species}{the species/taxa name(s) or identifier(s)}
 #'   \item{pres}{logical determining prescence (`1`) or absence (`0`) of the
 #'   species/taxa at that location}
-#'   \item{freq}{the localisied frequency for that species & neighbourhood before rescaling}
-#'   \item{freq_1}{the local frequency for that species & neighbourhood after rescaling}
+#'   \item{freq}{the localisied frequency for that species & neighbourhood
+#'   before rescaling}
+#'   \item{freq_1}{the local frequency for that species &
+#'   neighbourhood after rescaling}
 #'   \item{rank}{the original frequency rank}
 #'   \item{rank_1}{the frequency rank after rescaling}
 #'   \item{benchmark}{indicates whether the species was a benchmark
-#'   species for the local neighbourhood (`1` = benchmark
-#'   species/taxa)}
+#'   species for the local neighbourhood (`1` = benchmark species/taxa)}
 #'
 #' @export
 #'
-#' @description Determines the frequency scaling factors required in order to
-#'   attempt to correct for variation in recording effort.
+#' @importFrom foreach foreach
+#' @importFrom foreach %dopar%
+#' @importFrom foreach %do%
+#'
+#' @description Determines and applies frequency scaling factors in order to
+#'   attempt to correct for variation in recording effort in a biological
+#'   records dataset.
 #'
 #'
 #' @examples
@@ -61,34 +80,34 @@
 #'
 #'
 #'
-fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0.2703, missing_data = 2){
-  # This function calculates the sampling effort multiplier, alpha, that would equate sampling effort across all regions
-  # This is the main method of Frescalo.
+fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0.2703, filter_wts = FALSE, missing_data = 2){
+  # Determine all unique focal locations in neigh_wts and filter to locations in spp_pa if necessary
+    foc_locs = unique(neigh_wts$location1)
+    n_foc = length(foc_locs)
+    # Determine how many of these are not in the occupancy data
+    nd_inds = foc_locs[which(!foc_locs %in% all_loc)]
+    n_nd = length(nd_inds)
+    # If filter_wts TRUE the reduce neigh_wts to only rows where location1 is in all_loc
+    if(filter_wts){
+      neigh_wts = neigh_wts[which(neigh_wts$location1 %in% all_loc),]
+      # Redetermine foc_locs and n_foc after removal
+      foc_locs = unique(neigh_wts$location1)
+      n_foc = length(foc_locs)
+    } else {
+      if(n_nd > 10 & (n_nd/n_foc) >= 0.5)
+      warning(paste0("A large proportion of focal locations in 'neight_wts' are not present in 'spp_pa'\n"))
+    }
+    # Cleanup
+    rm(nd_inds, n_nd)
 
-  # The script returns a list containing two data frames: frescalo.out, freq.out
-  # frescalo.out contains the recorder-effort multiplier for each focal location, and other related information
-  # freq.out contains the corrected local frequency of each species in each neighborhood of a focal region,
-  # along with other related information
-  # Species identified as a benchmark in freq.out (benchmark=1) can be used to assess trends
-
-  # Argument missing_data defines what to do if data in the neighbourhood are missing.
-  # missing_data = 1 do not proceed with the calculation
-  # missing_data = 2 (default) set missing species data to all absences and proceed
-
-  # R_star defines the corrected frequency threshold for benchmark species
-  # (default = 0.27, means that after correction for recorder effort the top 27% of species are used as benchmarks)
-
-  # Determine all unique focal locations in neigh_wts
-  foc_locs = unique(neigh_wts$location1)
-  n_foc = length(foc_locs)
   # Calculate no. of spp for each focal location
-  nSpecies = sapply(spp_pa[match(foc_locs,all_loc,nomatch=length(all_loc)+1)], FUN=sum, simplify=T) # CH length(all_loc)+1 for nomatch so now if foc_loc not in all_loc then nSpecies = 0 should be returned (though due to filtering of weights dataset I don't think this should ever actually happen)
+    nSpecies = sapply(spp_pa[match(foc_locs,all_loc,nomatch=length(all_loc)+1)], FUN=sum, simplify=T) # CH length(all_loc)+1 for nomatch so now if foc_loc not in all_loc then nSpecies = 0 should be returned (though due to filtering of weights dataset I don't think this should ever actually happen)
   # Create data.frame to hold output location summary statistics for focal locs
-  out_loc = data.frame(
-    location=foc_locs,
-    nSpecies=nSpecies,
-    phi_in=NA, alpha=NA, phi_out=Phi, spnum_in=NA, spnum_out=NA, iter=NA
-  )
+    out_loc = data.frame(
+      location=foc_locs,
+      nSpecies=nSpecies,
+      phi_in=NA, alpha=NA, phi_out=Phi, spnum_in=NA, spnum_out=NA, iter=NA
+    )
 
   # Setup temporary object to hold location species frequency statistics (use temp list and collapse afterwards to avoid growing object)
   temp_freq = vector("list",n_foc)
@@ -97,6 +116,7 @@ fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0
   for(i_f in 1:n_foc){
     focal = foc_locs[i_f]
     focal_d = neigh_wts[which(neigh_wts$location1==focal),]
+    #cat("DEBUG: i_f = ",i_f," - ",focal,"\n",sep="")
 
     # Identify species in neighbourhood of focal region
     # If data missing for any neighbourhood locations for current focal location assign (e.g. nomatch) give it a index 1 greater than length of (spp_pa) so it will have a NULL value in speciesRegional
@@ -105,9 +125,14 @@ fresc_scaling = function(neigh_wts, spp_pa, all_loc, all_spp, Phi=0.74, R_star=0
     speciesRegional = lapply(speciesRegional, function(x) if(is.null(x)) rep(0, times = length(all_spp)) else x) # Find any locations in the neighbourhood that are not in spp_pa (should be NULL in speciesRegional) and create a spp_pa like vector for them with zeros for every species
 
     missingData = neighbourhood==length(spp_pa)+1 # length(spp_pa) is number of locations; missing data is neighbourhoods with
-    if (any(missingData) & missing_data==1) {
-      # Species data missing from a location in the neighbourhood. Ignore this focal location
-      warning(paste('Removing location ',focal,'. Missing data for locations in the neighbourhood.', sep=''))
+    if (all(missingData) | (any(missingData) & missing_data==1)){
+      if(all(missingData)){
+        # Species data missing from a location in the neighbourhood. Ignore this focal location
+        warning(paste('Removing location ',focal,'. No data for any location in the neighbourhood.', sep=''))
+      } else {
+        # Species data missing from a location in the neighbourhood. Ignore this focal location
+        warning(paste('Removing location ',focal,'. Missing data for locations in the neighbourhood.', sep=''))
+      }
 
       out_loc$alpha[i_f] = NA
       out_loc$iter[i_f] = NA
