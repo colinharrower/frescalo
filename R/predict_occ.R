cc22f5d3770cae20c9750484e200b9e97ceaa0aa
Colin Harrower <corr@ceh.ac.uk>
2024-08-13 10:10:26 +0100

    Add help file text for current predict_occ function

--- R/predict_occ.R
+++ R/predict_occ.R
@@ -1,26 +1,37 @@
-# Can this function work or be modified to be parallelised over more combinations/nodes than just the number of time periods
-
-# Addtional function for creating P_ijt values
+#' Predict Species Occupancies across locations and time
+#'
+#' @param freq the frequency results `data.frame` returned from `frescalo()`
+#' @param trend the trend results `data.frame` returned from `frescalo()`
+#'
+#' @return a data.frame containing the predicted occupancies (`p_occ`) for the
+#'   location, species, time combinations
+#' @export
+#'
+#' @description The outputs from the frescalo model are used to predict
+#' occupancies for each species, location, time combination.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' ## Run frescalo on the included example dataset
+#'   fres_res = frescalo(s,d,filter_wts = TRUE)
+#'
+#' ## Predict occupancies for all species,location,time combinations
+#'   p_occ = predict_occ(fres_res$freq, fres_res$trend)
+#' }
+#'
+#'
 predict_occ <- function(freq, trend){
-  list_data <- by(data = freq, as.factor(freq$location), function(x,trend){(1-(1-x$freq_1)^trend[trend$species==x$species,]$tFactor)}, trend = trend)
-  # Initialize variables
-  species <- vector("list", length(list_data))
-  IDs <- vector("list", length(list_data))
-  Values <- vector("list", length(list_data))
-  Labels <- vector("list", length(list_data))
-  labels <- unique(trend$time) # Label vector
-  # Populate the variables with data from the list (can this be done as vector calcs rather than loop?)
-  for (i in seq_along(list_data)) {
-    species[[i]] <- rep(unique(freq$species), times = length(list_data[[i]]))
-    IDs[[i]] <- rep(names(list_data)[i], times = length(list_data[[i]]))
-    Values[[i]] <- list_data[[i]]
-    Labels[[i]] <- labels
-  }
-  # Combine all elements into vectors
-  species <- unlist(species)
-  IDs <- unlist(IDs)
-  Values <- unlist(Values)
-  Labels <- unlist(Labels)
-  df <- data.frame(species = species, location = IDs, time = Labels, P_ijt = Values)
+  list_data <- by(
+    data = freq, freq[,c("location","species")],
+    function(x,trend){
+      cur_t = trend[trend$species==x$species,]
+      cur_p = (1-(1-x$freq_1)^cur_t$tFactor)
+      ret_obj = data.frame(species = x$species, location = x$location, time = cur_t$time, p_occ = cur_p)
+      return(ret_obj)
+    }, trend = trend, simplify = FALSE)
+  # Collapse list
+    df = do.call("rbind",list_data)
+  # Now return
   return(df)
 }
