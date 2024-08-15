9f24857f825c43ac6e385c989adce093718e946a
Colin Harrower <corr@ceh.ac.uk>
2024-08-15 16:50:30 +0100

    finishing first version of function to create neighbourhood weights dataset. Based on quick tests seems to match neighbourhood weights produced by Oli with exception that some location2 locations are listed in different orders where the weights are the same. Need to produce a wrapper version that can run in parallel to help when trying to produce weights datasets with more locations

--- R/create_wts.R
+++ R/create_wts.R
@@ -1,7 +1,7 @@
 dist_sf = function(sf_foc, sf_comp){
   # Convert to centroid
-    pt_foc = sf::st_centroid(sf_foc)
-    pt_comp = sf::st_centroid(sf_comp)
+    pt_foc = sf::st_centroid(sf::st_geometry(sf_foc))
+    pt_comp = sf::st_centroid(sf::st_geometry(sf_comp))
   # Return distance matrix
     pt_d = sf::st_distance(pt_foc, pt_comp)
   # Convert to numerical object
@@ -19,6 +19,28 @@ cosine_sim = function(df, foc_inds = NULL){
     sim = sim %*% t(sim)
   }
   return(sim)
+}
+
+wts_fun = function(nr,k,n){
+  # Formula on page 3 of Hill (2012)
+  # Create vectors
+    rs = w = rep(NA,nrow(nr))
+  # Rank distances
+    rd = rank(nr$d, ties.method = "first")
+  # Rank similarities
+    rs = rank(-nr$s, ties.method = "first")
+  # Determine which of the k nearest distances are in the top n in terms of similarity
+  # Note sort to keep original order
+    top_s = sort(order(nr$s,decreasing = TRUE)[1:n])
+  # subset rd and rsonly keeping ones in top_s
+    rd = rd[top_s]
+    rs = rs[top_s]
+  # Calc wts based on ranks
+    w = (1 - (rd-1)^2/k^2)^4 * (1 - (rs-1)^2/n^2)^4
+  # Build data.frame to return with indices of d/s that correspond to top_s
+    out_obj = data.frame(location2 = nr$location[top_s], w = w, row.names = NULL)
+  # Return weights
+    return(out_obj)
 }
 
 
@@ -40,10 +62,26 @@ calc_neigh_wts = function(foc_inds,comp_param, k = 200, n = 100){
   for(i in 1:length(foc_inds)){
     # Determine indices of k nearest locs
       nr_locs = order(dist_mat[i,])[1:k]
+    # Create data.frame to store neigh_stats
+      nr_stats = data.frame(location = comp_param$location[nr_locs], d = NA, s = NA)
+    # Extract distances for these
+      nr_stats[,"d"] = as.numeric(dist_mat[i,nr_locs])
+      #nr_d = dist_mat[i,nr_locs]
     # Now calculate similarities between these
-      nr_sim = cosine_sim(sf::st_drop_geometry(comp_param[nr_locs,par_cnames]),foc_inds[i])[1,]
-    # Now build data.frame of results
-      temp_wts[[i]] = data.frame(location1 = comp_param$location[foc_inds[i]], location2 = comp_param$location[nr_locs], w = nr_sim,row.names = NULL)
+      nr_stats[,"s"] = cosine_sim(
+        sf::st_drop_geometry(comp_param[nr_locs,par_cnames]),which(nr_locs == foc_inds[i])
+      )[1,] # First element of nr_locs should be foc_inds[i] but use which to be sure
+      #nr_sim = cosine_sim(sf::st_drop_geometry(comp_param[nr_locs,par_cnames]),foc_inds[i])[1,]
+    # Determine weights & add focal location to produce output in weights format
+      temp_wts[[i]] = data.frame(
+        location1 = comp_param$location[foc_inds[i]],
+        wts_fun(nr_stats, k, n),
+        row.names = NULL
+      )
   }
 
+  # Now collapse temp_wts list to a single data.frame
+    out_wts = do.call("rbind",temp_wts)
+  # Return weights
+    return(out_wts)
 }
