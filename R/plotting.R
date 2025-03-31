ef505eae0206f70ad674709d4e1202500a936fb3
colinharrower <corr@ceh.ac.uk>
2025-03-31 11:02:39 +0100

    modified plot_tfactor() so that y limit specification matches plot defaults and added ability for the user to specify/override the default y limits. If ylim not specified then default will use range of tfactor plus/minus sd. closes #5

--- R/plotting.R
+++ R/plotting.R
@@ -3,6 +3,7 @@
 #' @param sp_trend results from `trend` output of frescalo for a single species.
 #' @param ylab The y axis label
 #' @param xlab The x axis label
+#' @param y_lims =
 #' @param ... Any other arguments to be passed to `plot()` and or `arrows()`
 #'   functions used to create the plot or add the error bars
 #'
@@ -10,9 +11,11 @@
 #' @export
 #'
 #' @examples
-plot_tfactor = function(sp_trend, ylab = "Relative Frequency", xlab = "Time", ...){
-  y_lims = range(c(sp_trend$tFactor-sp_trend$StDev,sp_trend$tFactor+sp_trend$StDev))
-  plot(sp_trend[,c("time","tFactor")],ylim = y_lims, main = sp_trend$species[1], ylab = ylab, xlab = xlab, xaxt = "n",...)
+plot_tfactor = function(sp_trend, ylab = "Relative Frequency", xlab = "Time", ylim = NULL, ...){
+  if(is.null(ylim)){
+    ylim = range(c(sp_trend$tFactor-sp_trend$StDev,sp_trend$tFactor+sp_trend$StDev))
+  }
+  plot(sp_trend[,c("time","tFactor")],ylim = ylim, main = sp_trend$species[1], ylab = ylab, xlab = xlab, xaxt = "n",...)
   axis(1, at = sort(unique(sp_trend$time)))
   arrows(x0=sp_trend$time, x1=sp_trend$time, y0=sp_trend$tFactor-sp_trend$StDev, y1=sp_trend$tFactor+sp_trend$StDev, code = 3, angle = 90,length = 0.05,...)
 }
